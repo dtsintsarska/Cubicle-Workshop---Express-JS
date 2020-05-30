48d74c08d644e38f2abcebfce151a6635f507991
unknown <d.tsintsarska@gmail.com>
2020-05-30 21:09:35 +0300

    feat: create cube model

--- models/cubeModel.js
+++ models/cubeModel.js
@@ -1,13 +1,52 @@
+const {
+    v4
+} = require('uuid');
+const fs = require('fs');
+const path = require('path')
+
+let dbPath = path.join(__dirname, '..', 'config/database.json')
+
 class Cube {
     constructor(name, description, imageUrl, difficultyLevel) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.imageUrl = imageUrl;
-        this.difficultyLevel = difficultyLevel
+        this.id = v4();
+        this.name = name || 'No name';
+        this.description = description || 'Without description';
+        this.imageUrl = imageUrl || 'placeholder';
+        this.difficultyLevel = difficultyLevel || 0;
     }
 
     save() {
 
+        let newCube = {
+            id: this.id,
+            name: this.name,
+            description: this.description,
+            imageUrl: this.imageUrl,
+            difficultyLevel: this.difficultyLevel
+        }
+
+        fs.readFile(dbPath, (err, data) => {
+            if (err) {
+                throw err
+            }
+
+            console.log(data)
+            let db = JSON.parse(data)
+            db.push(newCube)
+
+            console.log(db)
+
+
+            fs.writeFile(dbPath, JSON.stringify(db), err => {
+                if (err) {
+                    throw err
+                }
+
+                console.log('Congrats! We have new Cube!')
+            })
+        })
+
     }
 }
+
+module.exports = Cube;
