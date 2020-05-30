053ba5030e7c0049ca60fee857dbc9d834f2a524
unknown <d.tsintsarska@gmail.com>
2020-05-30 20:01:58 +0300

    feat: initial project commit

---
+++ models/cubeModel.js
@@ -0,0 +1,13 @@
+class Cube {
+    constructor(name, description, imageUrl, difficultyLevel) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.difficultyLevel = difficultyLevel
+    }
+
+    save() {
+
+    }
+}
